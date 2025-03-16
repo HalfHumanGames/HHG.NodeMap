e98045c6d0075c89576ec704b760f7c3b0ce239a
snarlynarwhal <pjlegendreiv@gmail.com>
2025-03-16 00:29:54 -0400

    Misc cleanup

--- Runtime/Scripts/Components/NodeMapRenderer.cs
+++ Runtime/Scripts/Components/NodeMapRenderer.cs
@@ -39,7 +39,6 @@ namespace HHG.NodeMap.Runtime
         {
             if (nodeMapSettings != null)
             {
-                nodeMapSettings.Validate();
                 map = NodeMapGenerator.Generate(nodeMapSettings);
             }
         }
